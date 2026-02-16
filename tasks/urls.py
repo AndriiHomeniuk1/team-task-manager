3d3402d6b163e0efe5fcd9649287f2127000dd0c
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-02-16 23:18:14 +0200

    feat(tasks): add stub views and URL patterns
    Create stub views for:
    - index
    - Position
    - TaskType
    - Worker
    - Task
    
    Added URL patterns for each view in tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,2 +1,20 @@
+from django.urls import path
+
+from tasks.views import (
+    index,
+    PositionView,
+    TaskTypeView,
+    WorkerView,
+    TaskView,
+)
+
+
 app_name = "tasks"
-urlpatterns = []
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("positions/", PositionView.as_view(), name="position-list"),
+    path("task-types/", TaskTypeView.as_view(), name="task-type-list"),
+    path("workers/", WorkerView.as_view(), name="worker-list"),
+    path("tasks/", TaskView.as_view(), name="task-list"),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views import generic
 
-# Create your views here.
+from tasks.models import Position, TaskType, Worker, Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "tasks/index.html")
+
+class PositionView(generic.ListView):
+    model = Position
+
+class TaskTypeView(generic.ListView):
+    model = TaskType
+    template_name = "tasks/task_type_list.html"
+    context_object_name = "task_type_list"
+
+class WorkerView(generic.ListView):
+    model = Worker
+
+class TaskView(generic.ListView):
+    model = Task
