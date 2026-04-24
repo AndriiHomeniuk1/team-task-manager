1e12e47fa14b3f701e16599e74c614324ccc2f60
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-04-24 16:18:40 +0300

    chore(urls): serve media files in development
    - Import settings and static from django.conf
    - Add conditional static() for MEDIA_URL and MEDIA_ROOT when DEBUG is True

--- team_task_manager/urls.py
+++ team_task_manager/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tasks.urls", namespace="tasks"))
 ]
+
+if settings.DEBUG:  # only in development mode
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
